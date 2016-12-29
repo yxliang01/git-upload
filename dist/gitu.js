1bd5475092d7101c42a4b9bc95bdb297e006ab50
yxliang01 <13267.okk@gmail.com>
2016-12-29 02:51:47 +0100

    fix .gitignore

---
+++ dist/gitu.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _child_process = require("child_process");
+
+var _colors = require("colors");
+
+var _colors2 = _interopRequireDefault(_colors);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var CMD = function () {
+    function CMD(path, args) {
+        _classCallCheck(this, CMD);
+
+        this.path = path;
+        this.args = args;
+    }
+
+    _createClass(CMD, [{
+        key: "execute",
+        value: function execute(cb) {
+
+            var childProcess = (0, _child_process.spawn)(this.path, this.args, {
+                cwd: process.cwd(),
+                stdio: 'inherit'
+            });
+
+            childProcess.on('exit', function (code) {
+                if (code !== 0) {
+                    console.log(_colors2.default.red('git-upload failed.'));
+                    process.exit(1);
+                }
+
+                cb();
+            });
+        }
+    }]);
+
+    return CMD;
+}();
+
+new CMD('git', ['add', '.']).execute(function () {
+    new CMD('git', ['commit', '-m', process.argv[2] || '']).execute(function () {
+        new CMD('git', ['push']).execute(function () {
+            console.log(_colors2.default.green('done!'));
+        });
+    });
+});
--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@ Git-Upload
 [![Build Status](https://travis-ci.org/yxliang01/git-upload.svg?branch=master)](https://travis-ci.org/yxliang01/git-upload)
 [![Dependency Status](https://david-dm.org/yxliang01/git-upload.svg)]()
 [![Code Climate](https://codeclimate.com/github/yxliang01/git-upload/badges/gpa.svg)](https://codeclimate.com/github/yxliang01/git-upload)
+[![NSP Status](https://nodesecurity.io/orgs/git-upload/projects/1cee5cb2-7bd7-4909-b25b-6cb6634e75f3/badge)](https://nodesecurity.io/orgs/git-upload/projects/1cee5cb2-7bd7-4909-b25b-6cb6634e75f3)
 
 Save your time everyday
 
@@ -21,5 +22,11 @@ gitu MSG
 # Wait for prompts
 # When it has finished, it will prompt you "done!"
 ```
+
+<!-- To install manually(for developing purpose):
+```bash
+
+``` -->
+
 
 `gitu MSG` = `git add . && git commit -m MSG && git push`
